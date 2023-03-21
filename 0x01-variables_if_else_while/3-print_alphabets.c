cdcbe9d9e28508d5b145b0b71e81898af668a3ca
josephjona <josephjonathannyawara@gmail.com>
2023-03-20 19:16:21 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase except for "e" and "q".
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+
+letter++;
+}
+
+putchar('\n');
+return (0);
+}
+
