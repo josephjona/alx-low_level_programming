73646e6144745cf184127d4f3ba29c27b875b1a4
josephjona <josephjonathannyawara@gmail.com>
2023-03-25 08:45:21 -0700

    102fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,20 +8,23 @@
 int main(void)
 {
 int i = 0;
-long j = 0, k = 1, temp;
+long j = 1, k = 2;
 
 while (i < 50)
 {
-printf("%ld", k);
-if (i != 49)
-printf(", ");
-temp = k;
+if (i == 0)
+printf("%ld", j);
+else if (i == 1)
+printf(", %ld", k);
+else
+{
 k += j;
-j = temp;
-i++;
+j = k - j;
+printf(", %ld", k);
+}
+++i;
 }
 printf("\n");
 
 return (0);
 }
-
